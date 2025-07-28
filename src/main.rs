dc4607c2cf327a2cea8d4773c97407ebda248230
Oliver Webb <oliverwebb8192@gmail.com>
2025-07-28 18:16:31 -0500

    Hacked together garbage parser that works flawlessly

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::fs::File;
+use std::io::Bytes;
 #[warn(missing_docs)]
 use std::io::prelude::*;
 
-/// A purity file is a sequence of blocks.
+/// A purity file is a sequence of blocks surrounded by parens, brackets, curly brackets, or greater/less-than symbols
 enum PurityBlock {
     PlainText(String),
     SubjectHeader(String, usize),
@@ -13,7 +14,7 @@ enum PurityBlock {
 struct PurityParser<R: Read> {
     subject_number: usize,
     question_number: usize,
-    byte_source: R,
+    source: Bytes<R>,
 }
 
 impl<R: Read> PurityParser<R> {
@@ -21,7 +22,7 @@ impl<R: Read> PurityParser<R> {
         PurityParser {
             subject_number: 0,
             question_number: 0,
-            byte_source: input,
+            source: input.bytes(),
         }
     }
 }
@@ -31,21 +32,87 @@ impl<R: Read> Iterator for PurityParser<R> {
 
     fn next(&mut self) -> Option<Self::Item> {
         // All out-of-block characters are treated as comments
-        // TODO: For Now, Read the file as a block
-        if let Ok(s) = std::io::read_to_string(&mut self.byte_source) {
-            if s.len() > 0 {
-                return Some(PurityBlock::PlainText(s));
+
+        #[derive(Copy, Clone)]
+        enum Character {
+            Escaped(char),
+            Unescaped(char),
+        }
+
+        fn read_char<R: Read>(stream: &mut std::io::Bytes<R>) -> Option<Character> {
+            let byte = stream.next()?.ok()?;
+            if byte as char == '\\' {
+                Some(Character::Escaped(stream.next()?.ok()? as char))
             } else {
-                return None;
+                Some(Character::Unescaped(byte as char))
             }
-        } else {
-            None
+        }
+
+        fn is_char_important(c: Character) -> (bool, char) {
+            match c {
+                Character::Escaped(c) => (false, c),
+                Character::Unescaped(c) => match c {
+                    '[' => (true, '['),
+                    ']' => (true, ']'),
+                    '{' => (true, '{'),
+                    '}' => (true, '}'),
+                    '<' => (true, '<'),
+                    '>' => (true, '>'),
+                    '(' => (true, '('),
+                    ')' => (true, ')'),
+                    c => (false, c),
+                },
+            }
+        }
+
+        fn push_until<R: Read>(stream: &mut std::io::Bytes<R>, c: char, s: &mut String) {
+            // DO consume the ending char, since we know what it is and we don't want it
+            while let x = is_char_important(read_char(stream).unwrap()) {
+                if x == (true, c) {
+                    break;
+                }
+                s.push(x.1);
+            }
+        }
+
+        // I'm assuming ASCII since these test files are from 1989, TODO
+        let mut char_stream = &mut self.source;
+        let mut char_of_importance: (bool, char);
+        // Discard extra bytes before blocks
+        loop {
+            char_of_importance = is_char_important(read_char(char_stream)?);
+            if char_of_importance.0 {
+                break;
+            }
+        }
+        let mut text: String = String::new();
+        let inverse = match char_of_importance.1 {
+            '(' => ')',
+            '[' => ']',
+            '{' => '}',
+            '<' => '>',
+            _ => return None,
+        };
+        push_until(&mut char_stream, inverse, &mut text);
+        /// Give back the string
+        match inverse {
+            ')' => {
+                self.question_number += 1;
+                Some(PurityBlock::Question(text, self.question_number))
+            }
+            ']' => {
+                self.subject_number += 1;
+                Some(PurityBlock::SubjectHeader(text, self.subject_number))
+            }
+            '}' => Some(PurityBlock::PlainText(text)),
+            '>' => Some(PurityBlock::Conclusion(text)),
+            _ => None,
         }
     }
 }
 
 fn main() -> std::io::Result<()> {
-    let input = File::open("hacker")?;
+    let input = File::open("tests/hacker")?;
     let test: Vec<PurityBlock> = PurityParser::new(input).collect();
     for block in test {
         match block {
